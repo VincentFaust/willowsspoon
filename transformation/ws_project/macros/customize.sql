0db464d05678d256cb45f94a82acce8ec1d971f6
Vincent Faust <vincentffaust@gmail.com>
2023-04-22 17:04:33 -0500

    revert macro definition

--- transformation/ws_project/macros/customize.sql
+++ transformation/ws_project/macros/customize.sql
@@ -1,6 +1,8 @@
 {% macro generate_schema_name(custom_alias, node) -%}
     {%- if custom_alias == "staging" -%}
         staging
+    {%- elif custom_alias == "serving" -%}
+        serving
     {%- else -%}
         {{ target.schema }}_{{ custom_alias }}
     {%- endif -%}
