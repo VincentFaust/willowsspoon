774b9f2b76513325a8c1c89d68b6015a4b8a30c0
Vincent Faust <vincentffaust@gmail.com>
2023-04-22 17:05:03 -0500

    remove serving def

--- transformation/ws_project/macros/customize.sql
+++ transformation/ws_project/macros/customize.sql
@@ -1,9 +1,6 @@
 {% macro generate_schema_name(custom_alias, node) -%}
     {%- if custom_alias == "staging" -%}
         staging
-    {%- elif custom_alias == "serving" -%}
-        serving
-    {%- else -%}
         {{ target.schema }}_{{ custom_alias }}
     {%- endif -%}
 {% endmacro %}
