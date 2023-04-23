b86030019b5ea6899da0def379d27099cb240471
Vincent Faust <vincentffaust@gmail.com>
2023-04-22 19:50:44 -0500

    add macro to generate custom schemas

--- transformation/ws_project/macros/customize.sql
+++
@@ -1,6 +0,0 @@
-{% macro generate_schema_name(custom_alias, node) -%}
-    {%- if custom_alias == "staging" -%}
-        staging
-        {{ target.schema }}_{{ custom_alias }}
-    {%- endif -%}
-{% endmacro %}
