dbb10d870d0a7c41da2068be1c97eba71cfa467d
Vincent Faust <vincentffaust@gmail.com>
2023-06-20 12:13:54 -0500

    create seperate enviornments

--- transformation/ws_project/macros/generate_schema_name.sql
+++ transformation/ws_project/macros/generate_schema_name.sql
@@ -1,14 +1,17 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
-
     {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-
-        {{ default_schema }}
-
-    {%- else -%}
-
-        {{ custom_schema_name | trim }}
-
+    {% if target.name == "ci" %}
+        {%- if custom_schema_name is none -%}
+            pr_{{ env_var("GH_REF").split("pull/")[1].split("/")[0] if "pull" in env_var("GH_REF") else "" }}_{{ default_schema }}
+        {%- else -%}
+            pr_{{ env_var("GH_REF").split("pull/")[1].split("/")[0] if "pull" in env_var("GH_REF") else "" }}_{{ custom_schema_name | trim }}
+        {%- endif -%}
+    {% else %}
+        {%- if custom_schema_name is none -%}
+            {{ default_schema }}
+        {%- else -%}
+            {{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- endif -%}
 
 {%- endmacro %}
