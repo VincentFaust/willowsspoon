cddde66d6d33085004693d3584527342cdcf1548
Vincent Faust <vincentffaust@gmail.com>
2023-06-28 20:54:32 -0500

    remove price references

--- transformation/ws_project/models/sales/serving/fct_sales.sql
+++ transformation/ws_project/models/sales/serving/fct_sales.sql
@@ -2,9 +2,6 @@ with sales as (
     select
         _airbyte_unique_key
         , _airbyte_orders_hashid
-        , line_item_id
-        , price
-        , title
         , checkout_id
         , current_total_tax
         , order_number
@@ -17,19 +14,14 @@ with sales as (
         , last_name
         , date(created_at) as created_at
     from {{ ref("stg_orders") }}
-    inner join {{ ref("stg_orders_line_items") }}
-        using (_airbyte_orders_hashid)
 )
 
 ,
 final as (
     select
-        {{ dbt_utils.surrogate_key(["_airbyte_unique_key", "product_id", "line_item_id"]) }}
+        {{ dbt_utils.surrogate_key(["_airbyte_unique_key"]) }}
             as sales_key
-        , title
-        , product_id
         , order_number
-        , price
         , total_price
         , {{ dbt_utils.surrogate_key(["created_at"]) }} as created_at_key
         , {{ dbt_utils.surrogate_key(["address1","zip","city"]) }} as location_key
