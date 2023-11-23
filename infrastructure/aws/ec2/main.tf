46bd591e303561287a5878f85bd54d19981b604b
Vincent Faust <vincentffaust@gmail.com>
2023-11-22 20:39:15 -0600

    update ec2 settings

--- infrastructure/aws/ec2/main.tf
+++ infrastructure/aws/ec2/main.tf
@@ -32,3 +32,55 @@ resource "aws_dynamodb_table" "terraform_locks" {
     type = "S"
   }
 }
+
+resource "aws_instance" "ws_airbyte_production" {
+  ami           = "ami-0b0f111b5dcb2800f"
+  instance_type = "t2.medium"
+  key_name      = aws_key_pair.ws_key_pair.key_name
+
+  vpc_security_group_ids = [
+    aws_security_group.security_set.id,
+  ]
+
+  tags = {
+    name        = "ws_airbyte_production"
+    enviornment = "production"
+  }
+}
+
+resource "aws_key_pair" "ws_key_pair" {
+  key_name   = "ws-airbyte-pair"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "security_set" {
+  name        = "ec2-security-group"
+  description = "Allow inbound SSH traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_eip" "eip_integration" {
+  instance = aws_instance.ws_airbyte_production.id
+  vpc      = true
+}
