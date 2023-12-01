af0ee4225eecda77f0b3c34eb9522d8b8d8dadf6
Vincent Faust <vincentffaust@gmail.com>
2023-11-30 20:00:22 -0600

    update models

--- transformation/ws_project/models/sales/staging/sales/stg_orders.sql
+++ transformation/ws_project/models/sales/staging/sales/stg_orders.sql
@@ -8,18 +8,18 @@
 with source_data as (
     select
         created_at
-        , _airbyte_orders_hashid
-        , _airbyte_unique_key
-        , checkout_id
+        , _airbyte_ab_id
         , current_total_tax
         , order_number
         , total_price
-        , contact_email
-        , billing_address:zip::string as zip
-        , billing_address:address1::string as address1
-        , billing_address:city::string as city
-        , billing_address:first_name::string as first_name
-        , billing_address:last_name::string as last_name
+        , email
+        , shipping_address:zip::string as zip
+        , shipping_address:address1::string as address1
+        , shipping_address:city::string as city
+        , shipping_address:latitude::string as latitude
+        , shipping_address:longitude::string as longitude
+        , shipping_address:first_name::string as first_name
+        , shipping_address:last_name::string as last_name
     from {{ source("shopify", "orders") }}
 )
 
