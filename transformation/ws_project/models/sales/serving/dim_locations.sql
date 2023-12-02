e51568bba31fff462a500e2046a61719c26f45de
Vincent Faust <vincentffaust@gmail.com>
2023-12-01 21:48:18 -0600

    sql linting

--- transformation/ws_project/models/sales/serving/dim_locations.sql
+++ transformation/ws_project/models/sales/serving/dim_locations.sql
@@ -1,11 +1,11 @@
 with stg_locations as (
     select * from {{ ref("stg_locations") }}
-),
+)
 
-transformed as (
+, transformed as (
     select
-        {{ dbt_utils.surrogate_key(["address1", "city", "country", "zip"]) }} as location_key,
-        *
+        {{ dbt_utils.surrogate_key(["address1", "city", "country", "zip"]) }} as location_key
+        , *
     from stg_locations
 )
 
--- transformation/ws_project/models/sales/staging/location/stg_locations.sql
+++ transformation/ws_project/models/sales/staging/location/stg_locations.sql
@@ -1,18 +1,18 @@
 with source_data as (
     select distinct
-        parse_json(default_address):address1::string as address1,
-        parse_json(default_address):city::string as city,
-        parse_json(default_address):country::string as country,
-        parse_json(default_address):province::string as state,
-        parse_json(default_address):zip::string as zip
+        parse_json(default_address):address1::string as address1
+        , parse_json(default_address):city::string as city
+        , parse_json(default_address):country::string as country
+        , parse_json(default_address):province::string as state
+        , parse_json(default_address):zip::string as zip
     from {{ source("shopify", "customers") }}
 )
 
 select
-    row_number() over (order by (select null)) as unique_id
-    , address1
+    address1
     , city
     , country
     , state
     , zip
+    , row_number() over (order by (select null)) as unique_id
 from source_data
