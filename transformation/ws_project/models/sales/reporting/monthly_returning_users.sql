68c0ec9892da9e22ea478f868f0bb1dc85ac15b1
Vincent Faust <vincentffaust@gmail.com>
2023-12-05 13:26:34 -0600

    add user return model

---
+++ transformation/ws_project/models/sales/reporting/monthly_returning_users.sql
@@ -0,0 +1,32 @@
+with monthly_customers as (
+    select 
+        date_trunc('month', dates.date_day) as month
+        , sales.customer_identifier_key
+    from {{ref("fct_sales")}} sales 
+    join {{ref("dim_dates")}} dates 
+        on sales.created_at_key = dates.date_key
+    group by 
+        month, sales.customer_identifier_key
+)
+, previous_month_customers as (
+    select
+        month
+        , customer_identifier_key
+        , lag(customer_identifier_key) over (partition by customer_identifier_key order by month) as prev_month_customer
+    from monthly_customers
+)
+, return_rate as (
+    select
+        month
+        , count(distinct customer_identifier_key) as total_customers
+        , count(distinct case when prev_month_customer is not null then customer_identifier_key end) as returning_customers
+        , count(distinct case when prev_month_customer is not null then customer_identifier_key end)::float / nullif(count(distinct customer_identifier_key), 0) as return_rate
+    from 
+        previous_month_customers
+    group by 
+        month
+)
+select 
+    * 
+from return_rate
+order by month
