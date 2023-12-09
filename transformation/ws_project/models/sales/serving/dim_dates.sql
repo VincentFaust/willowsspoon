d63b9f0cffbe1d79580712f54fb4f35498471c0a
Vincent Faust <vincentffaust@gmail.com>
2023-12-09 17:36:58 -0600

    re add test comment

--- transformation/ws_project/models/sales/serving/dim_dates.sql
+++ transformation/ws_project/models/sales/serving/dim_dates.sql
@@ -3,7 +3,7 @@ with stg_date as (
 )
 
 ,
-
+--#
 transformed as (
     select
         {{ dbt_utils.surrogate_key(["date_day"]) }} as date_key
@@ -13,4 +13,3 @@ transformed as (
 
 select *
 from transformed
---#
